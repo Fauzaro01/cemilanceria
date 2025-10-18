6561a4e757a3ddba97f90ed48371508136f3b2f9
KinKafka <venom232013@gmail.com>
2025-10-18 18:07:58 +0700

    A
    tambah navbar (kin)

--- index.js
+++ index.js
@@ -8,9 +8,16 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs')
 
+
+// utama
 app.get('/', (req, res) => {
     res.render('index');
 })
+
+/* <halamanlogin */
+app.get('/login', (req, res) => {
+    res.render('login');
+});
 
 app.listen(port, () => {
     console.log("Server sudah berjalan pada port 3000")
