8135e64d7452a9eb105f42faeaadeefa340a8f38
fauzaro01 <muhamadfauzan4750@gmail.com>
2025-10-21 21:15:48 +0700

    feat: add dotenv configuration and update server to use environment variables

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
+require('dotenv').config({ quiet: true });
 const express = require('express');
 const logger = require('morgan');
-const path = require('path')
+const path = require('path');
 const app = express();
 
-const port = 3000;
-
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.use('/api', require('./route/api'));
 
 app.get('/', (req, res) => {
     res.render('index');
-})
+});
 
-app.listen(port, () => {
-    console.log("Server sudah berjalan pada port 3000")
-})
+app.listen(process.env.PORT, () => {
+    console.log(`[🚀] Server sudah berjalan pada http://localhost:${process.env.PORT}`);
+});
